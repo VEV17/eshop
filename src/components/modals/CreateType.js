7b96919036ddabd7498275b1c5dc50920271ae7e
VEV17 <veduhin@gmail.com>
2023-02-13 23:05:52 +0500

    Добавление типов девайсов!

--- src/components/modals/CreateType.js
+++ src/components/modals/CreateType.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Button, Form, FormControl, Modal } from "react-bootstrap";
+import { Context } from "../..";
 
 const CreateType = ({show, onHide}) => {
+    const {device} = useContext(Context);
+    const [value, setValue] = useState(''); 
+    const addType = () => {
+        device.types.push({id: device.types.length+1, name:value});
+        onHide();
+    }
+
     return (
         <Modal
             show={show}
@@ -16,14 +24,16 @@ const CreateType = ({show, onHide}) => {
             </Modal.Header>
             <Modal.Body>
                 <Form>
-                    <FormControl 
+                    <Form.Control
+                        value={value}
+                        onChange={e => setValue(e.target.value)} 
                         placeholder={"Введите название типа"}
                     />
                 </Form>
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="outline-danger" onClick={onHide}>Закрыть</Button>
-                <Button variant="outline-success" onClick={onHide}>Добавить</Button>
+                <Button variant="outline-success" onClick={addType}>Добавить</Button>
             </Modal.Footer>
         </Modal>
     )
